9f4b4b27205d08a66965631aee44d1489f3c906a
Misha Landau <landaumichael@yandex.ru>
2019-01-14 01:13:07 +0400

    init git pages.

--- src/modules/Sort.ts
+++
@@ -1,5 +0,0 @@
-export enum SortType {
-    ASC,
-    DESC,
-    DISABLE,
-}
--- src/modules/User.ts
+++
@@ -1,9 +0,0 @@
-namespace User {
-    export interface IUser {
-        id: number;
-        name: string;
-        surname: string;
-    }
-}
-
-export default User;
